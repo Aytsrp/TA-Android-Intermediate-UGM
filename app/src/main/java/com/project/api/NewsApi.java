c59b220cf7ee3c0ff3c9126a545f57e9801d1cf8
Aytsrp <ranukekeaja@gmail.com>
2023-10-04 16:19:01 +0700

    first commit

---
+++ app/src/main/java/com/project/api/NewsApi.java
@@ -0,0 +1,10 @@
+package com.project.api;
+
+public class NewsApi {
+    public static String GET_TOP_HEADLINES = "https://newsapi.org/v2/top-headlines?country=id&apiKey=4351a0e198454b228931304369881a6b";
+    public static String GET_CATEGORY_SPORTS = "https://newsapi.org/v2/top-headlines?country=id&category=sports&apiKey=4351a0e198454b228931304369881a6b";
+    public static String GET_CATEGORY_TECHNOLOGY = "https://newsapi.org/v2/top-headlines?country=id&category=technology&apiKey=4351a0e198454b228931304369881a6b";
+    public static String GET_CATEGORY_BUSINESS = "https://newsapi.org/v2/top-headlines?country=id&category=business&apiKey=4351a0e198454b228931304369881a6b";
+    public static String GET_CATEGORY_HEALTH = "https://newsapi.org/v2/top-headlines?country=id&category=health&apiKey=4351a0e198454b228931304369881a6b";
+    public static String GET_CATEGORY_ENTERTAINMENT = "https://newsapi.org/v2/top-headlines?country=id&category=entertainment&apiKey=4351a0e198454b228931304369881a6b";
+}
---
+++ app/src/main/java/com/project/ta_diklat/OpenNewsActivity.java
@@ -0,0 +1,14 @@
+package com.project.ta_diklat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OpenNewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_news);
+    }
+}
